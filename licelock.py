44b1741e50ed70b8df9bb0b1ad9cf9b718aee855
Glitch (python-os) <none>
2020-04-24 10:29:12 +0000

    🖐🗼 Checkpoint ./README.md:5152800/876 ./licelock.py:5152800/547

---
+++ licelock.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import logging
+
+
+
+def antipiracy():
+  logging.warn("this software utilises the EN_LICE_ULOCK mechanism")
+  sleep(2)
+  f = open("LICENSE.txt", "r")
+  sleep(2)
+  logging.info("anti-piracy test complete, execution permitted")
+  sleep(2)
