04fbc74e747a661200ebc4acb34242cdf0fc24b3
Glitch (python-os) <none>
2020-04-27 07:27:14 +0000

    🍗🚍 Checkpoint ./licelock.py:5152800/1389 ./main.py:5152800/46 ./requirements.txt:5152800/8

--- licelock.py
+++ licelock.py
@@ -13,6 +13,8 @@ now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 
+#-------------------------------------------------------------------
+
 def antipiracy():
   logging.warn("[" + current_time + "]" + ": this software utilises the EN_LICE_ULOCK mechanism, which is in all its glory useless")
   sleep(2)
@@ -22,3 +24,36 @@ def antipiracy():
   sleep(2)
   logging.error("[" + current_time + "]" + ": if script has not run, then we have detected a bad apple")
   logging.critical("[" + current_time + "]" + ": required file has not been found, please get a copy for the script to run")
+  
+#-------------------------------------------------------------------
+  
+def keylock():
+    from base64 import (
+        b64encode,
+        b64decode,
+    )
+
+    from Crypto.Hash import SHA256
+    from Crypto.Signature import PKCS1_v1_5
+    from Crypto.PublicKey import RSA
+
+
+    message = "Validated"
+    digest = SHA256.new()
+    digest.update(message)
+
+    # Read shared key from file
+    private_key = False
+    with open ("superpy.pem", "r") as myfile:
+        private_key = RSA.importKey(myfile.read())
+
+    # Load private key and sign message
+    signer = PKCS1_v1_5.new(private_key)
+    sig = signer.sign(digest)
+
+    # Load public key and verify message
+    verifier = PKCS1_v1_5.new(private_key.publickey())
+    verified = verifier.verify(digest, sig)
+    assert verified, ("Signature verification failed")
+    print("Successfully verified signature, booting...")
+    os.system("python3 boot.py")
